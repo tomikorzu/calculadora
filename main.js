91c9eb25135f96a41c88da89131fef57083e0c60
tomikorzu <tomykorzu@icloud.com>
2024-07-26 20:58:25 -0300

    feat(main.js): get all elements of the DOM and put them on variables

---
+++ main.js
@@ -0,0 +1,6 @@
+const op1 = document.getElementById('input1');
+const op2 = document.getElementById('input2');
+const op3 = document.getElementById('input3');
+const btnCalc = document.getElementById('calc');
+const btnReset = document.getElementById('reset');
+const results = document.getElementById('results');
